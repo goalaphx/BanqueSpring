a6f52727772adb78dd861d43a526ed9d6beab1ad
khalilh2002 <khalil.houssein2002@gmail.com>
2024-11-10 23:39:10 +0100

    before pull back mehdi

--- src/main/java/org/lsi/metier/CompteMetierImpl.java
+++ src/main/java/org/lsi/metier/CompteMetierImpl.java
@@ -1,4 +1,5 @@
 package org.lsi.metier;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -6,25 +7,29 @@ import org.lsi.dao.CompteRepository;
 import org.lsi.entities.Compte;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 @Service
 public class CompteMetierImpl implements CompteMetier {
 
-@Autowired
-private CompteRepository compteRepository;
+  @Autowired
+  private CompteRepository compteRepository;
 
-@Override
-public Compte saveCompte(Compte c) {
- // TODO Auto-generated method stub
- return compteRepository.save(c);
- } 
-@Override
-public List<Compte> listCompte() {
- // TODO Auto-generated method stub
- return compteRepository.findAll();
- }
- @Override
- public Compte getCompte(String codeCompte) {
-  Optional<Compte> compte = compteRepository.findById(codeCompte);
-  return compte.orElse(null); // Retourne le compte ou null si non trouvé
- }
-} 
+  @Override
+  public Compte saveCompte(Compte c) {
+    // TODO Auto-generated method stub
+    return compteRepository.save(c);
+  }
+
+  @Override
+  public List<Compte> listCompte() {
+    // TODO Auto-generated method stub
+    return compteRepository.findAll();
+  }
+
+  @Override
+  public Compte getCompte(String codeCompte) {
+    Optional<Compte> compte = compteRepository.findById(codeCompte);
+    return compte.orElse(null); // Retourne le compte ou null si non trouvé
+  }
+}
+
