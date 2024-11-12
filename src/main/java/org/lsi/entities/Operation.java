aaf24e70b588bba9f300b9b21ab34f095a4de758
El Mahdi Id Lahcen <idlahcen.elmahdi@etu.uae.ac.ma>
2024-11-12 01:24:35 +0100

    fixed operation type

--- src/main/java/org/lsi/entities/Operation.java
+++ src/main/java/org/lsi/entities/Operation.java
@@ -1,4 +1,5 @@
 package org.lsi.entities;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -12,67 +13,87 @@ import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Transient;
+
 @Entity
-@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(length=1)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(length = 1)
 public class Operation implements Serializable {
 
-@Id
-@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long numeroOperation;
+	private Date dateOperation;
+	private double montant;
 
-private Long numeroOperation;
-private Date dateOperation;
-private double montant;
+	@ManyToOne
+	@JoinColumn(name = "CODE_CPTE")
+	@JsonIgnore
+	private Compte compte;
 
+	@ManyToOne
+	@JoinColumn(name = "CODE_EMP")
+	@JsonIgnore
+	private Employe employe;
 
-@ManyToOne
-@JoinColumn(name="CODE_CPTE")
-@JsonIgnore
-private Compte compte;
-
-@ManyToOne
-@JoinColumn(name="CODE_EMP")
-@JsonIgnore  // Prevent recursive serialization of employee
-private Employe employe;
-
-
-public Operation(Date dateOperation, double montant) {
- super();
- this.dateOperation = dateOperation;
- this.montant = montant;
- }
-public Operation() {
- super();
- // TODO Auto-generated constructor stub
- }
-public Long getNumeroOperation() {
- return numeroOperation;
- } 
-public void setNumeroOperation(Long numeroOperation) {
-	 this.numeroOperation = numeroOperation;
-	 }
-	public Date getDateOperation() {
-	 return dateOperation;
-	 }
-	public void setDateOperation(Date dateOperation) {
-	 this.dateOperation = dateOperation;
-	 }
-	public double getMontant() {
-	 return montant;
-	 }
-	public void setMontant(double montant) {
-	 this.montant = montant;
-	 }
-	public Compte getCompte() {
-	 return compte;
-	 }
-	public void setCompte(Compte compte) {
-	 this.compte = compte;
-	 }
-	public Employe getEmploye() {
-	 return employe;
-	 }
-	public void setEmploye(Employe employe) {
-	 this.employe = employe;
-	 }
+	// Default constructor
+	public Operation() {
+		super();
 	}
+
+	// Parameterized constructor
+	public Operation(Date dateOperation, double montant) {
+		this.dateOperation = dateOperation;
+		this.montant = montant;
+	}
+
+	// Getters and setters for existing fields
+	public Long getNumeroOperation() {
+		return numeroOperation;
+	}
+
+	public void setNumeroOperation(Long numeroOperation) {
+		this.numeroOperation = numeroOperation;
+	}
+
+	public Date getDateOperation() {
+		return dateOperation;
+	}
+
+	public void setDateOperation(Date dateOperation) {
+		this.dateOperation = dateOperation;
+	}
+
+	public double getMontant() {
+		return montant;
+	}
+
+	public void setMontant(double montant) {
+		this.montant = montant;
+	}
+
+	public Compte getCompte() {
+		return compte;
+	}
+
+	public void setCompte(Compte compte) {
+		this.compte = compte;
+	}
+
+	public Employe getEmploye() {
+		return employe;
+	}
+
+	public void setEmploye(Employe employe) {
+		this.employe = employe;
+	}
+
+	// Transient method to expose operation type based on discriminator
+	@Transient
+	public String getType() {
+		if (this instanceof Retrait) return "Retrait";
+		if (this instanceof Versment) return "Versment";
+		if (this instanceof Virement) return "Virement";
+		return "Unknown";
+	}
+}
